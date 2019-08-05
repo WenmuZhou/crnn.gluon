43dcdb7137bcc6704530ac9cfd7d7b54ef6933f0
zhoujun <572459439@qq.com>
2019-08-05 17:37:47 +0800

    修复一个dataloader的bug

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -89,13 +89,13 @@ def get_dataloader(module_name, module_args, num_label):
     train_loader = DataLoader(dataset=train_dataset.transform_first(train_transfroms),
                               batch_size=module_args['loader']['train_batch_size'],
                               shuffle=module_args['loader']['shuffle'],
-                              last_batch='keep')
- #                             num_workers=module_args['loader']['num_workers'])
+                              last_batch='keep',
+                              num_workers=module_args['loader']['num_workers'])
 
     val_loader = DataLoader(dataset=val_dataset.transform_first(train_transfroms),
                             batch_size=module_args['loader']['train_batch_size'],
                             shuffle=module_args['loader']['shuffle'],
-                            last_batch='keep')
-#                            num_workers=module_args['loader']['num_workers'])
+                            last_batch='keep',
+                            num_workers=module_args['loader']['num_workers'])
 
     return train_loader, val_loader
