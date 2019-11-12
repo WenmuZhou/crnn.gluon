076f392989d14bdbd98ac567cd4eb828a117433f
zhoujun <572459439@qq.com>
2019-11-12 17:14:10 +0800

    修复一些bug

--- utils/create_lmdb_dataset.py
+++ utils/create_lmdb_dataset.py
@@ -4,12 +4,31 @@
 
 """ a modified version of CRNN torch repository https://github.com/bgshih/crnn/blob/master/tool/create_dataset.py """
 
-import fire
 import os
 import lmdb
 import cv2
-
+import pathlib
+from tqdm import tqdm
 import numpy as np
+
+
+def get_datalist(train_data_path):
+    """
+    获取训练和验证的数据list
+    :param train_data_path: 训练的dataset文件列表，每个文件内以如下格式存储 ‘path/to/img\tlabel’
+    :return:
+    """
+    train_data_list = []
+    for train_path in train_data_path:
+        for p in train_path:
+            with open(p, 'r', encoding='utf-8') as f:
+                for line in tqdm(f.readlines(), desc='reading data:' + p):
+                    line = line.strip('\n').replace('.jpg ', '.jpg\t').split('\t')
+                    if len(line) > 1:
+                        img_path = pathlib.Path(line[0])
+                        if img_path.exists() and img_path.stat().st_size > 0 and line[1]:
+                            train_data_list.append((line[0], line[1]))
+    return train_data_list
 
 
 def checkImageIsValid(imageBin):
@@ -29,7 +48,7 @@ def writeCache(env, cache):
             txn.put(k, v)
 
 
-def createDataset(inputPath, gtFile, outputPath, checkValid=True):
+def createDataset(data_list, outputPath, checkValid=True):
     """
     Create LMDB dataset for training and evaluation.
     ARGS:
@@ -42,22 +61,7 @@ def createDataset(inputPath, gtFile, outputPath, checkValid=True):
     env = lmdb.open(outputPath, map_size=1099511627776)
     cache = {}
     cnt = 1
-
-    with open(gtFile, 'r', encoding='utf-8') as data:
-        datalist = data.readlines()
-
-    nSamples = len(datalist)
-    for i in range(nSamples):
-        imagePath, label = datalist[i].strip('\n').split('\t')
-        imagePath = os.path.join(inputPath, imagePath)
-
-        # # only use alphanumeric data
-        # if re.search('[^a-zA-Z0-9]', label):
-        #     continue
-
-        if not os.path.exists(imagePath):
-            print('%s does not exist' % imagePath)
-            continue
+    for imagePath, label in tqdm(data_list, desc='make dataset, save to {}'.format(outputPath)):
         with open(imagePath, 'rb') as f:
             imageBin = f.read()
         if checkValid:
@@ -66,9 +70,6 @@ def createDataset(inputPath, gtFile, outputPath, checkValid=True):
                     print('%s is not a valid image' % imagePath)
                     continue
             except:
-                print('error occured', i)
-                with open(outputPath + '/error_image_log.txt', 'a') as log:
-                    log.write('%s-th image data occured error\n' % str(i))
                 continue
 
         imageKey = 'image-%09d'.encode() % cnt
@@ -79,13 +80,15 @@ def createDataset(inputPath, gtFile, outputPath, checkValid=True):
         if cnt % 1000 == 0:
             writeCache(env, cache)
             cache = {}
-            print('Written %d / %d' % (cnt, nSamples))
         cnt += 1
-    nSamples = cnt-1
+    nSamples = cnt - 1
     cache['num-samples'.encode()] = str(nSamples).encode()
     writeCache(env, cache)
     print('Created dataset with %d samples' % nSamples)
 
 
 if __name__ == '__main__':
-    fire.Fire(createDataset)
+    data_list = [["/media/zj/资料/zj/dataset/train_linux.csv"]]
+    data_list = get_datalist(data_list)
+
+    createDataset(data_list, '/media/zj/资料/zj/dataset/lmdb')
